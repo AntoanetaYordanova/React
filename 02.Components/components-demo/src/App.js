2903ec66604b54f2e84a85c91e3cd60cbd4fa732
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-05-17 14:05:03 +0300

    React

---
+++ 02.Components/components-demo/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">  
+        <h1>
+          ToDo App
+        </h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;

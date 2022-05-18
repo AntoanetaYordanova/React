14a19cda388891dc78edf7089df72f75665cc69b
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-05-18 13:01:34 +0300

    React

--- 02.Components/components-demo/src/App.js
+++ 02.Components/components-demo/src/App.js
@@ -9,10 +9,15 @@ function App() {
         </h1>
       </header>
       <main>
-
+        <h2>Tasks</h2>
+        <ul>
+          <li>Clean your room</li>
+          <li>Go shopping</li>
+          <li>Learn React</li>
+        </ul>
       </main>
       <footer>
-        
+        <p>All rights reserved &copy;</p>
       </footer>
     </div>
   );

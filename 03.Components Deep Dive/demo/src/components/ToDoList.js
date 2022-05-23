9a123943aa0e4c3279ac5e1069dc75156c077af2
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-05-23 18:07:03 +0300

    React

--- 03.Components Deep Dive/demo/src/components/ToDoList.js
+++ 03.Components Deep Dive/demo/src/components/ToDoList.js
@@ -1,14 +1,65 @@
-import ToDoLi from "./TodoItem"
+import ToDoLi from './TodoItem';
+import { useState, useEffect } from 'react';
+import uniqId from 'uniqid';
+
+const API_URL = 'https://happymindapi.herokuapp.com/'
 
 export default function ToDoList() {
-    const todos = [
-        { id : 1, content: 'Clean my room'},
-        { id : 2, content: 'Wash the dishes'},
-        { id : 3, content: 'Go to the gym'}
-    ]
-    return(
-        <ul>
-            {todos.map(x => <ToDoLi  key={x.id} props = {x}/>)}
-        </ul>
-    )
+    const [todos, setTodos] = useState([
+        { id: uniqId(), content: 'Clean my room', isDone: false },
+        { id: uniqId(), content: 'Wash the dishes', isDone: false },
+        { id: uniqId(), content: 'Go to the gym', isDone: false },
+    ]);
+
+    const addItem = (e) => {
+        const todo = {
+            id: uniqId(),
+            content: e.target.value,
+            isDone: false,
+        };
+        setTodos((oldTodos) => [...oldTodos, todo]);
+    };
+
+    const deleteTodoItemClickHandler = (id) => {
+        setTodos((oldTodos) => oldTodos.filter((x) => x.id !== id));
+    };
+
+    const toggleTodoItemClickHandler = (id) => {
+        setTodos((oldTodos) => {
+            const selectedTodo = oldTodos.find((x) => x.id === id);
+            const toggledTodo =  {...selectedTodo, isDone : !selectedTodo.isDone}
+            const selectedTodoIndex = todos.indexOf(selectedTodo);
+            const newTodos = [...oldTodos.slice(0, selectedTodoIndex), toggledTodo, ...oldTodos.slice(selectedTodoIndex + 1)];
+            return newTodos
+        });
+    };
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        fetch(API_URL + 'articles')
+        .then(response => response.json())
+        .then(result => setArticles(result))
+        .catch(err => console.log(err));
+        console.log('Mounted');
+    }, []);
+
+    console.log(articles);
+    return (
+        <>
+            <ul>
+                <input type="text" onBlur={addItem} />
+                {todos.map((x) => (
+                    <ToDoLi
+                        key={x.id}
+                        id={x.id}
+                        content={x.content}
+                        onDelete={deleteTodoItemClickHandler}
+                        onClick={toggleTodoItemClickHandler}
+                        isDone = {x.isDone}
+                    />
+                ))}
+            </ul>
+        </>
+    );
 }
--- 03.Components Deep Dive/demo/src/components/TodoItem.js
+++ 03.Components Deep Dive/demo/src/components/TodoItem.js
@@ -1,5 +1,17 @@
-export default function ToDoLi({props}) {
+// import {useEffect} from 'react';
+import './TodoItem.css'
+import styles from './TodoItemModule.module.css'
+
+export default function ToDoLi({id, content, onDelete, onClick, isDone}) {
+    // useEffect(() => {
+    //     console.log(props.id + ' - Monunted');
+    //     return () => console.log(props.id + ' - Unmounted');
+    // }, [])
     return(
-        <li>{props.content}</li>
+        <>
+            <li className={isDone ? 'done' : ''}  onClick={() => onClick(id)}>{content}</li>
+            {/* <li className={styles.liColorExample} onClick={() => onClick(id)}>{content}</li> */}
+            <button onClick={() => onDelete(id)}>X</button>
+        </>
     )
 }

299e874fef6c3642c920db6846e47365ea1f662c
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-06-17 18:22:36 +0300

    React

--- 05.Forms/forms-demo/src/App.js
+++ 05.Forms/forms-demo/src/App.js
@@ -1,15 +1,35 @@
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+const [data, setData] = useState([]);
+const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:3030/jsonstore/services')
+        .then(res => res.json())
+        .then(res => setData(Object.values(res)));
+    }, []);
+
+    function onServiceChange (e) {
+        setUsername(e.target.value);
+    }
+
+    function onUsernameChange(e) {
+        const username = e.target.value;
+        setUsername(username);
+    }
+
     return (
         <div className={styles.app}>
             <div>
             <h3>Login Form</h3>
             <form action="" method="post">
                 <div>
-                    <label htmlFor="username">
+                    <label htmlFor="username" >
                         Username:
-                        <input type="text" name="username" id="" />
+                        <input type="text" name="username" id=""  value={username || 'Pesho'} onChange={onUsernameChange}/>
                     </label>
                 </div>
                 <div>
@@ -18,6 +38,12 @@ function App() {
                         <input type="password" name="password" id="" />
                         <input type="submit" value="Login" />
                     </label>
+                </div>
+                <div>
+                    <label htmlFor="services">Services</label>
+                    <select name="services" id="services" onChange={onServiceChange}>
+                        {data.map(d => <option key={d._id} value={d._id}>{d.name}</option>)}
+                    </select>
                 </div>
             </form>
             </div>
